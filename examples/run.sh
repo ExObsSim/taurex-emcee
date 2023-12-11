80d8d4b0285baf4d3231aa700719dc61639cdb95
andrea.bocchieri <andrea.bocchieri@uniroma1.it>
2023-12-11 14:42:03 +0100

    updated

--- examples/run.sh
+++ examples/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-taurex -i emcee_selfretrieval_gpu.par -R -o ../output/emcee_selfretrieval.h5
-taurex-plot -i ../output/emcee_selfretrieval.h5 -o ../output/plots --all
+# taurex -i parfile.par -R -o selfretrieval.h5
+mpirun -np 16 taurex -i parfile.par -R -o selfretrieval.h5
+taurex-plot -i selfretrieval.h5 -o /plots --all
