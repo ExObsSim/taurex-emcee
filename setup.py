ba32fcda0d8114137008425a05a0d631dcf9b1d3
andrea.bocchieri <andrea.bocchieri@uniroma1.it>
2023-12-11 14:43:54 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ entry_points = {"taurex.plugins": "emcee = taurex_emcee"}
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.2.1-alpha"
+version = "0.3.0-alpha"
 
 setup(
     name="taurex_emcee",
